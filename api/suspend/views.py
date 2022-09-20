55083e41aa8317ec2c552389ee3b1c68d9f7b713
twyle <lyceokoth@gmail.com>
2022-09-20 12:54:24 +0300

    feat: creates suspend swagger ui

--- api/suspend/views.py
+++ api/suspend/views.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 """This module contains all the routes used to suspend articles and suspends."""
+from flasgger import swag_from
 from flask import Blueprint, jsonify
 
 suspend = Blueprint("suspend", __name__)
 
 
-@suspend.route("/", methods=["GET"])
-def get_suspend():
-    """Get a an suspend by id."""
-    return jsonify({"suspend": "one"}), 200
+@swag_from(
+    "./docs/suspend_author.yml", endpoint="suspend.suspend_author", methods=["POST"]
+)
+@suspend.route("/author", methods=["POST"])
+def suspend_author():
+    """Suspend Author with given id."""
+    return jsonify({"suspend": "author"}), 200
+
+
+@swag_from(
+    "./docs/suspend_article.yml", endpoint="suspend.suspend_article", methods=["POST"]
+)
+@suspend.route("/article", methods=["POST"])
+def suspend_article():
+    """Suspend article with given id."""
+    return jsonify({"suspend": "article"}), 200
