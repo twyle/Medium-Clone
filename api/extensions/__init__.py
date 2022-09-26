ffd044b169e6326ed205763cc6c1539f8f6f7369
twyle <lyceokoth@gmail.com>
2022-09-26 08:06:53 +0300

    refactor: uses __init__ to expeort package

---
+++ api/extensions/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""Export all the extensions."""
+from .extensions import cors, db, ma, migrate, register_extensions, swagger
+
+__all__ = ["cors", "swagger", "db", "ma", "migrate", "register_extensions"]
